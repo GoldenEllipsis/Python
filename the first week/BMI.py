d510325b8ab4e9e71dbe12140c15f427e3668e7a
GoldenEllipsis <tsnotary@sina.com>
2017-03-22 19:24:59 +0800

    Update BMI.py

--- the first week/BMI.py
+++ the first week/BMI.py
@@ -1,17 +1,26 @@
 '''
 BMI   Body Mass Index
-国内标准
-	体重kg，身高m
-国际标准：BMi 24改成25，28改成30
+国内标准 国际标准
+体重kg，身高m
 '''
 weight = input("请输入体重：\n")
 high = input("请输入身高：\n")
 BMI = eval(weight)/pow(eval(high),2)
 if BMI<18.5:
-	print("偏瘦")
-elif BMI>=18.5 and BMI<24:
-	print("正常")
-elif BMI>=24 and BMI<28: 
-	print("偏胖")
-elif BMI>=28:            
-	print("肥胖")
+    print("国内标准：偏瘦")
+    print("国际标准：偏瘦")
+elif BMI>=18.5 and BMI<25:
+    print("国际标准：正常")
+    if BMI>=18.5 and BMI<24:
+        print("国内标准：正常")
+    elif BMI>=24:
+        print("国内标准：偏胖")
+elif BMI>=25 and BMI<30:
+    print("国际标准：偏胖")
+    if BMI>=25 and BMI<28:
+        print("国内标准：偏胖")
+    elif BMI>=28:
+        print("国内标准：肥胖")
+elif BMI>=30:
+    print("国内标准：肥胖")
+    print("国内标准：肥胖")
